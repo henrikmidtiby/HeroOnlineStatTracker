38d8abb9bf1f7503b9439df0ecca58102a964c48
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2020-04-25 10:09:38 +0200

    Clean up imports.

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 # Copyright: Henrik Skov Midtiby 2019.
-import collections
 import logging
-from flask import Flask, render_template, request, session, redirect, url_for, send_from_directory
-import html
-from flask_socketio import SocketIO, join_room, leave_room
-from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from werkzeug.security import generate_password_hash, check_password_hash
 import os
-import base64
-import time
-import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -19,16 +11,9 @@ app = Flask(__name__, static_folder='build')
 app.config['SECRET_KEY'] = os.urandom(20)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
-                                        'sqlite:///' + os.path.join(basedir, 'app.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 gunicorn_logger = logging.getLogger("gunicorn.error")
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 socketio = SocketIO(app)
 
