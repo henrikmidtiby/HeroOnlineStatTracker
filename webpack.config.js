faa92f9e99b02f8da2f32de3f3e3a2051ed2b797
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2024-06-26 11:03:04 +0200

    Simplified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+
 module.exports = {
   entry: {
-    webpack_bundle: ['./components/hero-stats.js', 
-                     './node_modules/socket.io-client/dist/socket.io.js'], 
+    webpack_bundle: ['./components/hero-stats.js',
+                    ], 
   },
   output: {
     filename: '[name].js',
